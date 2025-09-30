715ebb825c345a109b22e7f12f9fff6b7a80d7ac
gvilherme <55121824+gvilherme@users.noreply.github.com>
2025-09-30 13:53:47 +0000

    chore(terraform): fmt (auto)

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -21,8 +21,8 @@ data "aws_caller_identity" "me" {}
 # VPC default (baixo custo)
 data "aws_vpc" "default" { default = true }
 data "aws_subnets" "default" {
-  filter { 
-      name = "vpc-id" 
-      values = [data.aws_vpc.default.id] 
-    }
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
